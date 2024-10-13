0ad393d39448d8f135c9984ebc9e2a5cd194aba0
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-10-13 11:46:08 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+let itemcount = 1
+
+function itemlist(count) {
+	const list =document.getElementById("infi-list");
+	for(i=0;i<count;i++){
+		const li = document.createElement("li");
+		     li.textContent = `list item ${itemcount}`
+		itemcount++
+		list.appendChild(li)
+	}
+}
+
+function scroll(){
+	const list =document.getElementById("infi-list");
+	list.scrollTop + list.clientHeight >= list.scrollHeight - 1
+	itemlist(2)
+}
+
+itemlist(10)
+
+document.getElementById("infi-list").addEventListener("scroll",scroll)
